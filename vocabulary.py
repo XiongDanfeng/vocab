d07741ebceaeb24ed07a0bfe9906b593ff0aabdd
Danfeng Xiong <xiong.danfeng@gmail.com>
2024-09-14 16:38:02 +0200

    add vocabulary.py

---
+++ vocabulary.py
@@ -0,0 +1,40 @@
+words = {
+    "mince": [("to cut food, especially meat, into very small pieces, often using a special machine:",
+              [])],
+    "desert": [],
+    "surrogate": [("a person who acts or speaks in support of someone else, or does his or her job for a certain time:",
+                   ["High-profile surrogates like that speaking on behalf of Harris"])]
+    "espouse": [("to support an activity or opinion:",
+                 ["Harris is now embracing cutting red tape and cutting taxes, principles that have traditionally been espoused by Republicans. ",
+                  ])]
+}
+
+nonelist = {
+    "furtherance": [("",["a double down of ideology, a kind of furtherance of a MAGA viewpoint"])],
+}
+
+verblist = {
+"engender": [("",["who engenders confidence or could do the job"])],
+}
+
+advlist = {
+
+
+}
+
+phrase = {
+    "mince no words": ("to speak in a very direct and honest way without worrying about offending someone",
+                       ""),
+
+}
+
+phrase = ["Listen to what they said.",
+          "hell a lot of",
+          "move the niedle",
+          "part of the calculus",
+          "a metric for",
+          "does that matter?"
+          ]
+gram_sentences = [
+    "interest rates for businesses have been high just as they have been for consumers. "
+]
