437f72d29971b1709fdc93e20815a1ab823806cd
Danfeng Xiong <xiong.danfeng@gmail.com>
2024-09-15 15:00:44 +0200

    add Wortschatz.py

---
+++ Wortschatz.py
@@ -0,0 +1,14 @@
+
+
+
+
+verblist = {
+"vertagen": [("auf einen späteren Tag verschieben; aufschieben",
+              ["Ob der Einsatz dieser Waffen gegen Ziele auf russischem Territorium grundsätzlich  erlaubt werden soll, diese Entscheidung haben \
+                US-Präsident Biden und der britische Premier  vertagt."])],
+}
+
+phrase = {
+    'alles auf eine Karte setzen': [('alles riskieren', ['Ukraine setzt alles auf eine Karte'])]
+
+}
