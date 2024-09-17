2770622d3e15d11b3a515c15ee597778d1fdef5b
Danfeng Xiong <xiong.danfeng@gmail.com>
2024-09-17 08:37:02 +0200

    September 13, 2024

--- vocabulary.py
+++ vocabulary.py
@@ -1,3 +1,4 @@
+# [('', [])]
 words = {
     "mince": [("to cut food, especially meat, into very small pieces, often using a special machine:",
               [])],
@@ -6,19 +7,39 @@ words = {
                    ["High-profile surrogates like that speaking on behalf of Harris"])],
     "espouse": [("to support an activity or opinion:",
                  ["Harris is now embracing cutting red tape and cutting taxes, principles that have traditionally been espoused by Republicans. ",
-                  ])]
+                  ])],
+    'disavow': [('deny any responsibility or support for.', ['all things which President Trump has disavowed,'])]
 }
 
+# [('', [])]
 nonelist = {
     "furtherance": [("",["a double down of ideology, a kind of furtherance of a MAGA viewpoint"])],
+    'geese ':[('a large waterbird with a long neck, short legs, webbed feet, and a short broad bill. Generally geese are larger than ducks and have longer necks and shorter bills.',[])],
+    'affiliate': [('',[])],
 }
-
+# [('', [])]
 verblist = {
-"engender": [("",["who engenders confidence or could do the job"])],
+    "engender": ("",["who engenders confidence or could do the job"]),
+    'smear': ('damage the reputation of (someone) by false accusations', ['she is tired of watching the radical left Marxists and fascists violently attack and smear me']),
+    'debunk': ('expose the falseness or hollowness of (an idea or belief).', [' the Republican governor of Ohio all debunked this story about people eating pets',]),
+    'diehard': ('continuing to support something in spite of opposition',[]),
+    'foresee':  [('to know about something before it happens:', [])],
+    'concur': [('be of the same opinion; agree.', ['..what she concurred with'])]
 }
-
+# [('', [])]
+adjlist = {
+    'oblique': [('having a sloping direction, angle, or position', ['Trump talks about these things in very oblique ways. '])],
+}
+# [('', [])]
 advlist = {
 
+
+}
+
+idomlist = {
+    "in light of sth": (' because of certain knowledge now in hand; considering something', [
+        'I think people are more willing now in light of the hate and division that we see coming out of Donald Trump'
+    ]),
 
 }
 
@@ -28,13 +49,24 @@ phrase = {
 
 }
 
-phrase = ["Listen to what they said.",
+verbphrase = {
+    'peel off': [('remove a thin outer covering or layer.', [
+        'her travel to these more rural in an attempt to peel off voters from former President Donald Trump.'
+    ])],
+}
+
+adverbphrase = [
+    'in an attempt to',
+]
+
+shotphrase = ["Listen to what they said.",
           "hell a lot of",
           "move the niedle",
           "part of the calculus",
           "a metric for",
-          "does that matter?"
+          "does that matter?",
           ]
+
 gram_sentences = [
     "interest rates for businesses have been high just as they have been for consumers. "
 ]
